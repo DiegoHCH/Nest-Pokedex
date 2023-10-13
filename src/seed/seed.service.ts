57f24b626a7d6e078a558ec1f88b71bdf69113c3
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-10-13 10:35:48 -0500

    Create a custom provider

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,21 +3,22 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { FetchAdapter } from 'src/common/adapters/fetch.adapter';
 
 @Injectable()
 export class SeedService {
 
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel : Model<Pokemon>
+    private readonly pokemonModel : Model<Pokemon>,
+    private readonly http : FetchAdapter
   ){}
 
   async executeSeed() {
 
     await this.pokemonModel.deleteMany({});
 
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=650");
-    const data: PokeResponse = await response.json();
+    const data: PokeResponse = await this.http.get("https://pokeapi.co/api/v2/pokemon?limit=650");
 
     const pokemonToInsert: {name:string, no: number}[] = [];
 
