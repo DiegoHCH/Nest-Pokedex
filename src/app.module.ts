7bb6c574fcb9477e4124100831209b278e944c79
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-10-13 11:18:55 -0500

    Setting environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -9,13 +10,18 @@ import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    
+  }
+}
