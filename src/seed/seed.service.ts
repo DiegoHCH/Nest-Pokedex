91e83d42c88afb9a6517c02eb139eceac4b75da7
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-10-13 10:02:50 -0500

    Insert Pokemons per batch

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel : Model<Pokemon>
+  ){}
 
   async executeSeed() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
     const data: PokeResponse = await response.json();
 
-    data.results.map(({name, url}) => {
+    data.results.map(async ({name, url}) => {
       const no = +url.split('/').at(-2);
-      console.log(name, no) ;
+      const pokemon = await this.pokemonModel.create({name, no});
     })
     
-    return data.results;
+    return 'Seed Execute Succesfully';
   }
 
 }
