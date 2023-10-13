066f0599c87c0ae5db0153018d367c7b21b97a4c
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-10-13 09:49:25 -0500

    Make http request from Nest

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
 
-  executeSeed() {
-    return 'Seed Executed';
+  async executeSeed() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+    const data: PokeResponse = await response.json();
+
+    data.results.map(({name, url}) => {
+      const no = +url.split('/').at(-2);
+      console.log(name, no) ;
+    })
+    
+    return data.results;
   }
 
 }
